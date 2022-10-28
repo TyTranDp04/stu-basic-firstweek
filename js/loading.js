ab8cc41353af0000133dfbdecd81ce10cc98c9a4
TyTranDp04 <ty.tran_dp04@devplus.edu.vn>
2022-10-29 00:10:18 +0700

    Fix css

--- js/loading.js
+++ js/loading.js
@@ -3,5 +3,7 @@ const setTimeoutLoading = setTimeout(showPage, 2000);
 function showPage() {
   document.getElementById("loading").style.display = "none";
   document.getElementById("loading-icon").style.display = "none";
+  document.getElementById("header").style.display = "block";
   document.getElementById("content").style.display = "block";
+  document.getElementById("footer").style.display = "block";
 };
